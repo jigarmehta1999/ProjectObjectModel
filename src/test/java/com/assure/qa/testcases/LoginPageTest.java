c2c2ce6a632e13a28467b5605e72861eaccdb1e6
Jigar Mehta Git <jigar.digital@gmail.com>
2019-08-06 01:22:30 +0530

    Signed-off-by: Jigar Mehta Git <jigar.digital@gmail.com>

--- src/test/java/com/assure/qa/testcases/LoginPageTest.java
+++ src/test/java/com/assure/qa/testcases/LoginPageTest.java
@@ -1,13 +1,19 @@
 package com.assure.qa.testcases;
 
+import java.util.Hashtable;
+
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import com.assure.qa.base.TestBase;
 import com.assure.qa.pages.HomePage;
 import com.assure.qa.pages.LoginPage;
+import com.assure.qa.util.XLS_POI;
+import com.assure.qa.util.dataProvider;
 
 
 public class LoginPageTest extends TestBase{
@@ -67,16 +73,21 @@ public class LoginPageTest extends TestBase{
 	}
 	
 	
-	@Test(priority=3)
-	public void loginSuccessfulTest(){
+	@Test(priority=3,dataProvider="getLoginTestData")
+	public void loginSuccessfulTest(Hashtable<String, String> testdata, ITestContext context){
 		sa = new SoftAssert();
 		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
 		sa.assertEquals(homePage.verifyUserName(), "Hello, Abhilashaa");
-		sa.assertEquals(homePage.verifyEmailID(), "abhilasha.jha@northgateps.com");
+//		sa.assertEquals(homePage.verifyEmailID(), "abhilasha.jha@northgateps.com");
 		sa.assertAll();
 	}
 	
 	
+	@DataProvider
+	public Object[][] getLoginTestData() {
+		XLS_POI xlsx = new XLS_POI("D:\\Eclipse SimRel WorkSpace\\ProjectObjectModel\\src\\main\\java\\com\\assure\\qa\\testdata\\AssureTestData.xlsx"); 
+		return (dataProvider.getData(xlsx, "LoginTest", "LoginTest"));
+	}
 	
 	@AfterMethod
 	public void tearDown(){
